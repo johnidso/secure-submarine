f3511112c617be37b6bdf1b5971b7e198f547d15
John Idso <john.d.idso@gmail.com>
2021-07-27 19:49:10 -0500

    base mode

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,11 +3,10 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // what is the value of req.user????
   console.log('req.user:', req.user);
 
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(`SELECT * FROM "secret" WHERE secrecy_level < $1;`, [req.user.clearance_level])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
